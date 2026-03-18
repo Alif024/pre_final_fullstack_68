4ad841492af89460a146d484d5e0bcc0248e35b7
Alif024 <aleefrock12345@gmail.com>
2026-03-18 21:32:38 +0700

    #

--- frontend/app.js
+++ frontend/app.js
@@ -12,6 +12,7 @@ const BACKEND_URL = `http://localhost:${process.env.BACKEND_PORT || 3000}`;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
